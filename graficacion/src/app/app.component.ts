adca8c720e3e21bd460a50217efeae83966b1214
puppy09 <zepedaramireztics@gmail.com>
2025-02-05 00:38:43 -0700

    proyecto base angular graficacion

--- graficacion/src/app/app.component.ts
+++ graficacion/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuPrincipalComponent } from "./componentes/menu-principal/menu-principal.component";
-import { UmlCasosUsoComponent } from './componentes/uml-casos-uso/uml-casos-uso.component';
-import { UmlPaquetesComponent } from './componentes/uml-paquetes/uml-paquetes.component';
-import { UmlSecuenciasComponent } from './componentes/uml-secuencias/uml-secuencias.component';
-import { UmlClasesComponent } from './componentes/uml-clases/uml-clases.component';
-import { UmlComponentesComponent } from './componentes/uml-componentes/uml-componentes.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuPrincipalComponent, UmlCasosUsoComponent, UmlPaquetesComponent, UmlSecuenciasComponent, UmlComponentesComponent, UmlClasesComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
